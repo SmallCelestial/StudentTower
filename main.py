b33d056786765367ece9c646720b6a4044841007
ppaczek04 <ppaczek1@o2.pl>
2024-07-08 10:27:21 +0200

    checker

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+main_screen=pygame.display.set_mode((800,400)) #width,height
+pygame.display.set_caption("Pixel_Runner")
+
+main_clock=pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            exit()
+       
+    
+    pygame.display.update()
+    main_clock.tick(60) #maximum framerate
