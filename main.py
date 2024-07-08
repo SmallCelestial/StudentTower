36eda51ea648de172ec496a6e812674cd752411b
ppaczek04 <ppaczek1@o2.pl>
2024-07-08 10:40:54 +0200

    m122

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ main_screen=pygame.display.set_mode((800,400)) #width,height
 pygame.display.set_caption("Pixel_Runner")
 
 main_clock=pygame.time.Clock()
-# asfadfas
+new_Variable=100
 
 while True:
     for event in pygame.event.get():
