2369e35d587e03f3e264d2cea3f5937a685e2afb
Piotr Paczek <ppaczek1@o2.pl>
2024-07-10 15:42:32 +0200

    changes to naming convention in main.py file

--- main.py
+++ main.py
@@ -15,34 +15,34 @@ class Player(pygame.sprite.Sprite):
         self.rect=self.image.get_rect(midbottom=(500,200)) #jakbys zmienial to napisz
 
         #basic_parameters
-        self.x_speed=0
-        self.y_speed=0
-        self.current_height=0
-        self.max_height=0
+        self.x_speed = 0
+        self.y_speed = 0
+        self.current_height = 0
+        self.max_height = 0
         floor_under_legs_status=True #if true player stands on floor and can jump
 
 
     def player_input(self):
         keys=pygame.key.get_pressed()
         if (keys[pygame.K_UP] or keys[pygame.K_SPACE]):
-            self.y_speed=-10
+            self.y_speed =- 10
         if keys[pygame.K_DOWN]:
-            self.y_speed=+10
+            self.y_speed =+ 10
         if keys[pygame.K_LEFT]:
-            self.rect.x-=5 
+            self.rect.x -= 5 
         if keys[pygame.K_RIGHT]:
-            self.rect.x+=5
+            self.rect.x += 5
 
 
     def gravity_function(self):
-        if self.rect.bottom >= 700 and self.current_height==0:
-            self.rect.bottom=700
+        if self.rect.bottom >= 700 and self.current_height == 0:
+            self.rect.bottom = 700
         
-        if self.y_speed<10: #max garivty strength
-            self.y_speed+=0.2 #gravity strength
+        if self.y_speed < 10: #max garivty strength
+            self.y_speed += 0.2 #gravity strength
 
-        self.rect.centerx+=self.x_speed
-        self.rect.centery+=self.y_speed
+        self.rect.centerx += self.x_speed
+        self.rect.centery += self.y_speed
 
     def contact_with_steps(self,steps):
         pass #TO BE IMPLEMENTED
@@ -52,49 +52,62 @@ class Player(pygame.sprite.Sprite):
         self.player_input()
         self.gravity_function()
 
-
-#CLASSES
+"""
+STEP_SNOWBIOM
+===Atributes===
+relative_height -> it describes height on screen, 1000 is at top, 0 is at bottom (contrary to y. positioning)
+falling_speed ->  how many pixels a sec a step goes down
+height -> height of a step
+width -> wifth of a step
+topLeft -> a list of two elements, first being x cord, second y cord of topleft corner of step
+topRight -> a list of two elements, first being x cord, second y cord of topright corner of step
+image -> image, a texture of an object
+rect -> rectangle of an object
+===Methods===
+falling_mechcanic() -> makes step fall, based on relative height, and also kill when step out of screen
+animation_mechanic() -> responsible for changing textures for image variable, makes step animated
+update() -> function updating status of  created objects
+"""
 class step_snowbiom(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.floor_snowbiom_300_0=pygame.image.load('resources/floors/floor_snowbiom_300_0.png').convert_alpha()
-        self.floor_snowbiom_300_1=pygame.image.load('resources/floors/floor_snowbiom_300_1.png').convert_alpha()
-        self.floor_snowbiom_300_2=pygame.image.load('resources/floors/floor_snowbiom_300_2.png').convert_alpha()
-        self.floor_snowbiom_300_3=pygame.image.load('resources/floors/floor_snowbiom_300_3.png').convert_alpha()
+        self.floor_snowbiom_300_0 = pygame.image.load('resources/floors/floor_snowbiom_300_0.png').convert_alpha()
+        self.floor_snowbiom_300_1 = pygame.image.load('resources/floors/floor_snowbiom_300_1.png').convert_alpha()
+        self.floor_snowbiom_300_2 = pygame.image.load('resources/floors/floor_snowbiom_300_2.png').convert_alpha()
+        self.floor_snowbiom_300_3 = pygame.image.load('resources/floors/floor_snowbiom_300_3.png').convert_alpha()
         self.floor_snowbiom_300=[self.floor_snowbiom_300_0,self.floor_snowbiom_300_1,self.floor_snowbiom_300_2,self.floor_snowbiom_300_3]
         self.floor_snowbiom_index=0
         
         
         #basic_parameters
-        self.relative_height=1000 # t desribes height on screen, when =0, step disappear
-        self.falling_speed=5 #1000 is at top, 0 is at bottom (contrary to y. positioning)
-        self.height=50
-        self.width=300
+        self.relative_height = 1000 
+        self.falling_speed = 5  #1000 is at top, 0 is at bottom (contrary to y. positioning)
+        self.height = 50
+        self.width = 300
 
         #setting topleft adn topright points, needed for function checking if player standing on sth
-        self.topLeft=[random.randint(300,600),0]
-        self.topRight=[self.topLeft[0]+self.width,self.topLeft[1]]
+        self.topLeft = [random.randint(300,600),0]
+        self.topRight = [self.topLeft[0]+self.width,self.topLeft[1]]
 
-        self.image= self.floor_snowbiom_300_0
-        self.rect=self.image.get_rect(topleft=self.topLeft)
+        self.image = self.floor_snowbiom_300_0
+        self.rect = self.image.get_rect(topleft=self.topLeft)
 
         
     def falling_mechanic(self):
-        self.rect.centery+=self.falling_speed
-        self.relative_height-=self.falling_speed
-        if self.relative_height<=0:
+        self.rect.centery += self.falling_speed
+        self.relative_height -= self.falling_speed
+        if self.relative_height <= 0:
             self.kill()
         
-        #updating topLeft and topRight cords
-        self.topLeft[1]=self.relative_height #x cords, stay unchanged
-        self.topRight[1]=self.relative_height #x cords, stay unchanged
+        self.topLeft[1] = self.relative_height #x cords, stay unchanged
+        self.topRight[1] = self.relative_height #x cords, stay unchanged
         
 
     def animation_mehcanic(self):
-        self.floor_snowbiom_index+=0.02
-        if self.floor_snowbiom_index>=len(self.floor_snowbiom_300):
-            self.floor_snowbiom_index=0
-        self.image=self.floor_snowbiom_300[int(self.floor_snowbiom_index)]
+        self.floor_snowbiom_index += 0.02
+        if self.floor_snowbiom_index >= len(self.floor_snowbiom_300):
+            self.floor_snowbiom_index = 0
+        self.image = self.floor_snowbiom_300[int(self.floor_snowbiom_index)]
 
     def update(self):
         self.falling_mechanic()
@@ -105,15 +118,15 @@ class step_snowbiom(pygame.sprite.Sprite):
 main_clock=pygame.time.Clock()
 game_status="game_on" #intro, game_on, outro
 
-floor_spawn_cooldown=60 #time between each step is spawned
-floor_spawn_timer=floor_spawn_cooldown #timer used for measuring time between spawns of steps
+floor_spawn_cooldown = 60 #time between each step is spawned
+floor_spawn_timer = floor_spawn_cooldown #timer used for measuring time between spawns of steps
 
 #GLOBAL_FUNCTIONS
 def spawning_steps(): #function responsible for cyclic spawning falling steps
     global floor_spawn_timer
     global floor_spawn_cooldown
-    floor_spawn_timer-=1
-    if floor_spawn_timer<=0:
+    floor_spawn_timer -= 1
+    if floor_spawn_timer <= 0:
         falling_floors_group.add(step_snowbiom())
         floor_spawn_timer=floor_spawn_cooldown
 
@@ -127,7 +140,7 @@ start_floor=pygame.image.load('resources/floors/start_floor.png').convert_alpha(
 
 
 #GROUPS
-player_group=pygame.sprite.GroupSingle()
+player_group = pygame.sprite.GroupSingle()
 player_group.add(Player())
 
 falling_floors_group=pygame.sprite.Group()
@@ -135,7 +148,7 @@ falling_floors_group=pygame.sprite.Group()
 
 while True:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
