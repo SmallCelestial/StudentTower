c124f0b6c830de7038ac9025a9619116bd572046
Piotr Paczek <ppaczek1@o2.pl>
2024-07-11 12:16:39 +0200

    stairs_mech_being_implemented

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 LEFT_WALL_COORDINATE = 100
 RIGHT_WALL_COORDINATE = 900
 
@@ -10,7 +11,7 @@ class Player(pygame.sprite.Sprite):
         self.player_walk = pygame.image.load("resources/Player/player_stand.png").convert_alpha()
 
         self.image = self.player_walk  # jakbys zmienial to napisz
-        self.rect = self.image.get_rect(midbottom=(500, 200))  # jakbys zmienial to napisz
+        self.rect = self.image.get_rect(midbottom=(500, 900))  # jakbys zmienial to napisz
 
         # basic_parameters
         self.x_speed = 0
@@ -34,15 +35,20 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += 5
 
     def gravity_function(self):
-        if self.rect.bottom >= 700 and self.current_height == 0:
+        if self.rect.bottom >= 700 and self.current_height <= 0:
             self.rect.bottom = 700
 
-        if self.y_speed < 10:  # max garivty strength
+        if self.y_speed < 10 and self.floor_under_legs_status == False:  # max garivty strength
             self.y_speed += 0.2  # gravity strength
+        else:
+            self.y_speed = 0
 
         self.rect.centerx += self.x_speed
         self.rect.centery += self.y_speed
 
+    def height_status(self):
+        self.current_height -= self.y_speed
+        self.max_height = max(self.max_height, self.current_height)
 
     # temporary function
     def contact_with_floor(self):
@@ -53,3 +59,5 @@ class Player(pygame.sprite.Sprite):
         self.player_input()
         self.gravity_function()
         self.contact_with_floor()
+        self.height_status()
+        print(self.current_height) # for testing purposes
